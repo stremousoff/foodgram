88939c5c51f01278a77ceca6b6d8c5c3623f194f
Anton Stremousov <stremousoff@gmail.com>
2024-08-27 22:45:32 +0300

    review_v3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -106,10 +106,6 @@ else:
         }
     }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
