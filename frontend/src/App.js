e386a8b593e3596ca63589da7766d731a97c7bad
Anton Stremousov <stremousoff@gmail.com>
2024-08-29 03:05:37 +0300

    review_v4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -294,7 +294,7 @@ function App() {
             </Route>
 
             <Route exact path="/about">
-              {<About component={About}/> }
+              {<About component={About} />}
             </Route>
 
             <Route exact path="/reset-password">
@@ -302,7 +302,7 @@ function App() {
             </Route>
 
             <Route exact path="/technologies">
-              {<Technologies component={Technologies}/> }
+              {<Technologies component={Technologies}/>}
             </Route>
 
             <Route exact path="/recipes">
--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -41,7 +41,7 @@ const About = ({ updateOrders, orders }) => {
               Код проекта находится тут - <a href="https://github.com/stremousoff/foodgram" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="https://github.com/stremousoff" className={styles.textLink}>Стремоусов Антон</a>
+              Автор проекта: <a href="https://github.com/stremousoff/" className={styles.textLink}>Антон Стремоусов</a>
             </p>
           </div>
         </aside>
